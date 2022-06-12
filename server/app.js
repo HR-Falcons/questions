e6dd127db86b790006264a16976de673fc3b284b
arkteck <arkteck@gmail.com>
2022-06-12 13:57:04 -0400

    Loader.io verification

--- server/app.js
+++ server/app.js
@@ -1,16 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const db = require('./db.js');
 
 const app = express();
 
 app.use(express.json());
-app.use((req, res, next) => {
-  console.log(req.method, req.url);
-  next();
-});
 app.use(cors());
+app.use(express.static(path.join(__dirname, '..')));
 
 app.get('/', (req, res) => {
   res.status(200).send(`
