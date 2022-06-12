f4c44543e8064f0639989c69ccfe75bb8e64f674
arkteck <arkteck@gmail.com>
2022-06-12 14:20:58 -0400

    Console log middleware back in

--- server/app.js
+++ server/app.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, '..')));
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+});
 
 app.get('/', (req, res) => {
   res.status(200).send(`
