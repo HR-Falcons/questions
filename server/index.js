01ee7d28a1538f16bf29e02cb0f5bed36b74286c
arkteck <arkteck@gmail.com>
2022-06-12 10:55:13 -0400

    Added error message to port listening

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
+require('dotenv').config();
 const { app } = require('./app.js');
 
-app.listen(process.env.PORT);
-
-console.log(`Listening at http://localhost:${process.env.PORT}`);
+app.listen(process.env.PORT, (err) => {
+  if (err) {
+    console.log('Error listening to port,', err);
+  } else {
+    console.log(`Listening to port ${process.env.PORT}`);
+  }
+});
